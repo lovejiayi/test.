d4d4cb27810c5175e37666584d2898165df7a2af
anzhongyuan <732162183@qq.com>
2021-12-05 15:48:04 +0800

    function(3)

---
+++ 函数递归3/函数递归3/2021 12 02.c
@@ -0,0 +1,42 @@
+#define  _CRT_SECURE_NO_WARNINGS
+//求第n个斐波那契数
+//#include<stdio.h>
+//int Fib(int n)
+//{
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 2) + Fib(n - 1);
+//
+//}
+#include<stdio.h>
+int Fib(int n)
+{
+	int a = 1;
+	int b = 1;
+	int c = 1;
+	while (n > 2)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		n--;
+	}
+
+	return c;
+}
+
+
+int main()
+{
+	int n = 0;
+	int ret = 0;
+	scanf("%d", &n);
+	ret = Fib(n);
+	printf("ret=%d", ret);
+	return 0;
+
+
+
+
+}
